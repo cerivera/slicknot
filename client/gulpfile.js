35533c8d8afb5182e95ade73124c8537a18901ae
Carlos Eduardo Rivera <cerivera@fastmail.fm>
2015-01-24 19:58:04 -0600

    minor changes

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -12,20 +12,36 @@ var livereload = require('gulp-livereload');
 var del = require('del');
 
 
-gulp.task('styles', function() {
+gulp.task('styles-dev', function() {
+    return gulp.src(['src/vendor/css/**/*.css', 'src/slicknot/css/**/*.css'])
+        .pipe(concat('main.css'))
+        .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('scripts-dev', function() {
+    return gulp.src(['src/vendor/js/angular.js', 'src/vendor/js/modules/**/*.js', 'src/slicknot/js/**/*.js'])
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('styles-prod', function() {
     return gulp.src(['src/vendor/css/**/*.css', 'src/slicknot/css/**/*.css'])
         .pipe(concat('main.css'))
         .pipe(minifycss())
-        .pipe(gulp.dest('dist/assets/css'));
+        .pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('scripts', function() {
+gulp.task('scripts-prod', function() {
     return gulp.src(['src/vendor/js/**/*.js', 'src/slicknot/js/**/*.js'])
         .pipe(concat('main.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('dist/assets/js'));
+        .pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('default', function() {
-    gulp.start('styles', 'scripts');
+    gulp.start('styles-dev', 'scripts-dev');
+});
+
+gulp.task('prod', function() {
+    gulp.start('styles-prod', 'scripts-prod');
 });
