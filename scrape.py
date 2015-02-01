c1ad6eab4a85047a57e60a6e05863ed0edc6d963
Carlos Eduardo Rivera <cerivera@fastmail.fm>
2015-02-01 16:36:41 -0600

    new, awesome code

--- scrape.py
+++ scrape.py
@@ -1,27 +1,57 @@
-import asyncio
-import aiohttp
 import json
+import keys
+import requests
+import csv
+import StringIO
+import re
+import time
 
-API_URL = 'https://www.kimonolabs.com/api/6htta1k8?apikey=yMslDnCVP4I9LZDZg25U1O29zwCvtGrR'
-
-@asyncio.coroutine
-def get(*args, **kwargs):
-    response = yield from aiohttp.request('GET', *args, **kwargs)
-    return (yield from response.read_and_close())
-
-
-@asyncio.coroutine
-def print_slickdeals():
-    result = yield from get(API_URL)
-    json_result = json.loads(result.decode())
-
-    print(json_result['thisversionrun'])
-    for deal in json_result['results']['collection1']:
-        print("%s:%s"%(deal['fp_titles']['text'], deal['fp_titles']['href']))
+CACHE_EXPIRE_SECONDS = 60*60*24
+LOOP_SECONDS = 60*60
 
 
+def fetch_deals(cache):
+    response = requests.get(keys.KIMONO_URL)
+    _json = json.loads(response.text)
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(print_slickdeals())
+    for deal in _json['results']['collection1']:
+        text = deal['fp_titles']['text'].strip()
+        link = deal['fp_titles']['href'].strip()
 
+        if cache.exists(text):
+            cache.expire(text, CACHE_EXPIRE_SECONDS) 
+        else:
+            cache.setex(text, json.dumps({
+                'link': link, 
+                'notified': []
+            }), CACHE_EXPIRE_SECONDS)
+
+
+def run_queries(cache):
+    response = requests.get(keys.QUERIES_URL)
+    _csv = csv.reader(StringIO.StringIO(response.text))
+    c.next()
+    count = 0
+    for row in c:
+        email = row[0].strip()
+        query = row[1].strip()
+        _regex = re.compile(query)
+        for key in cache.keys():
+            if _regex.match(key):
+                json_val = json.loads(cache.get(key))
+                if email not in json_val['notified']:
+                    # TODO Send notification email
+                    json_val['notified'].append(email)
+                    cache.setex(key, json.dumps(json_val), cache.ttl(key))
+        count += 1
+
+    print("Queries: %s" % count)
+
+if __name__ == '__main__':
+    cache = redis.Redis(host='localhost', port=6379, db=0)
+
+    while True:
+        deals = fetch_deals(cache)
+        queries = run_queries(cache)
+        time.sleep(LOOP_SECONDS)
 
