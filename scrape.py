4ac23bca16725e1ef849647674b0ae74b502699f
Carlos Eduardo Rivera <cerivera@fastmail.fm>
2015-02-02 07:57:44 -0600

    Updating requirements and removing debug

--- scrape.py
+++ scrape.py
@@ -6,13 +6,13 @@ import StringIO
 import re
 import time
 import redis
+import mandrill
 
 CACHE_EXPIRE_SECONDS = 60*60*24
 LOOP_SECONDS = 60*60
 
 
 def fetch_deals(cache):
-    print("Fetching deals")
     response = requests.get(keys.KIMONO_URL)
     _json = json.loads(response.text)
 
@@ -30,31 +30,23 @@ def fetch_deals(cache):
 
 
 def run_queries(cache):
-    print("running queries")
     response = requests.get(keys.QUERIES_URL)
     _csv = csv.reader(StringIO.StringIO(response.text))
     _csv.next()
-    count = 0
     for row in _csv:
-        print(row)
-        email = row[0].strip()
-        query = row[1].strip()
+        query = row[0].strip()
         _regex = re.compile(query, re.IGNORECASE)
         for key in cache.keys():
             if _regex.search(key):
-                print("Matched %s" % key)
                 json_val = json.loads(cache.get(key))
-                if email not in json_val['notified']:
-                    print("Notifying %s" % email)
+                if keys.NOTIFICATION_EMAIL not in json_val['notified']:
                     # TODO Send notification email
-                    json_val['notified'].append(email)
+                    json_val['notified'].append(keys.NOTIFICATION_EMAIL)
                     cache.setex(key, json.dumps(json_val), cache.ttl(key))
-        count += 1
 
-    print("Queries: %s" % count)
 
 if __name__ == '__main__':
-    cache = redis.Redis(host='localhost', port=6379, db=0)
+    cache = redis.Redis(host=keys.REDIS_HOST, port=keys.REDIS_PORT, db=0)
 
     while True:
         deals = fetch_deals(cache)
