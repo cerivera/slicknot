2ec3f856bd0b612c1613d78e4addda21adbf11bd
Carlos Rivera <cerivera@fastmail.fm>
2015-02-08 14:43:04 -0600

    Fixing dev server config

--- slicknot.py
+++ slicknot.py
@@ -1,7 +1,4 @@
 from flask import Flask
-# Only requirement from Flask constructor is the name of the main module.
-# Flask uses this to determine the root path of the app 
-# so that it can find resource files relative to the location of the app.
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,6 +6,4 @@ def hello():
     return "Hello, World!"
 
 if __name__ == '__main__':
-    # If you do'nt include '0.0.0.0', this application won't 
-    # be visible from outside of this machine (e.g. host machine).
-    app.run(host='0.0.0.0');
+    app.run()
