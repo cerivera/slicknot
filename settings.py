81df6cfb5b22da9ce3e54b3ed04e1e65b9b9d42a
Carlos Rivera <cerivera@fastmail.fm>
2015-02-23 20:16:06 -0600

    Adding two versions for slickdeals and fixing URLs

--- settings.py
+++ settings.py
@@ -4,57 +4,63 @@ MANDRILL_API_KEY = 'xtmDrnGEtCALiXvcIFA5Uw'
 
 ENDPOINTS = [
     {
-        # Slickdeals - Front Page
-        'status': 0,
-        'api': 'https://www.kimonolabs.com/api/6htta1k8?apikey=yMslDnCVP4I9LZDZg25U1O29zwCvtGrR',
+        # Slickdeals - Front Page (1.0)
+        'status': 1,
+        'api': 'https://www.kimonolabs.com/api/json/bpjl89bg?apikey=yMslDnCVP4I9LZDZg25U1O29zwCvtGrR',
+        'csv': 'https://dl.dropboxusercontent.com/u/7156802/apps/slickdeals.csv'
+    },
+    {
+        # Slickdeals - Front Page (2.0)
+        'status': 1,
+        'api': 'https://www.kimonolabs.com/api/json/6htta1k8?apikey=yMslDnCVP4I9LZDZg25U1O29zwCvtGrR',
         'csv': 'https://dl.dropboxusercontent.com/u/7156802/apps/slickdeals.csv'
     },
     {
         # OfferUp - Tools
         'status': 0,
-        'api': 'https://www.kimonolabs.com/api/25nkauhs?apikey=yMslDnCVP4I9LZDZg25U1O29zwCvtGrR',
+        'api': 'https://www.kimonolabs.com/api/json/25nkauhs?apikey=yMslDnCVP4I9LZDZg25U1O29zwCvtGrR',
         'csv': 'https://dl.dropboxusercontent.com/u/7156802/apps/tools.csv'
     },
     {
         # OfferUp - Furniture
         'status': 0,
-        'api': 'https://www.kimonolabs.com/api/6r0p70yq?apikey=yMslDnCVP4I9LZDZg25U1O29zwCvtGrR',
+        'api': 'https://www.kimonolabs.com/api/json/6r0p70yq?apikey=yMslDnCVP4I9LZDZg25U1O29zwCvtGrR',
         'csv': 'https://dl.dropboxusercontent.com/u/7156802/apps/furniture.csv'
     },
     {
         # OfferUp - Sporting Goods
         'status': 1,
-        'api': 'https://www.kimonolabs.com/api/8t9webp2?apikey=yMslDnCVP4I9LZDZg25U1O29zwCvtGrR',
+        'api': 'https://www.kimonolabs.com/api/json/8t9webp2?apikey=yMslDnCVP4I9LZDZg25U1O29zwCvtGrR',
         'csv': 'https://dl.dropboxusercontent.com/u/7156802/apps/sports.csv'
     },
     {
         # OfferUp - Bikes
         'status': 1,
-        'api': 'https://www.kimonolabs.com/api/b1lsf7f2?apikey=yMslDnCVP4I9LZDZg25U1O29zwCvtGrR',
+        'api': 'https://www.kimonolabs.com/api/json/b1lsf7f2?apikey=yMslDnCVP4I9LZDZg25U1O29zwCvtGrR',
         'csv': 'https://dl.dropboxusercontent.com/u/7156802/apps/bikes.csv'
     },
     {
         # Craigslist - Materials
         'status': 0,
-        'api': 'https://www.kimonolabs.com/api/2wb1jebu?apikey=yMslDnCVP4I9LZDZg25U1O29zwCvtGrR',
+        'api': 'https://www.kimonolabs.com/api/json/2wb1jebu?apikey=yMslDnCVP4I9LZDZg25U1O29zwCvtGrR',
         'csv': 'https://dl.dropboxusercontent.com/u/7156802/apps/materials.csv'
     },
     {
         # Craigslist - Sporting Goods
         'status': 1,
-        'api': 'https://www.kimonolabs.com/api/8pqgrnq4?apikey=yMslDnCVP4I9LZDZg25U1O29zwCvtGrR',
+        'api': 'https://www.kimonolabs.com/api/json/8pqgrnq4?apikey=yMslDnCVP4I9LZDZg25U1O29zwCvtGrR',
         'csv': 'https://dl.dropboxusercontent.com/u/7156802/apps/sports.csv'
     },
     {
         # Craigslist - Bikes
         'status': 1,
-        'api': 'https://www.kimonolabs.com/api/4iqw51jk?apikey=yMslDnCVP4I9LZDZg25U1O29zwCvtGrR',
+        'api': 'https://www.kimonolabs.com/api/json/4iqw51jk?apikey=yMslDnCVP4I9LZDZg25U1O29zwCvtGrR',
         'csv': 'https://dl.dropboxusercontent.com/u/7156802/apps/bikes.csv'
     },
     {
         # Craigslist - Tools
         'status': 0,
-        'api': 'https://www.kimonolabs.com/api/3biahcyu?apikey=yMslDnCVP4I9LZDZg25U1O29zwCvtGrR',
+        'api': 'https://www.kimonolabs.com/api/json/3biahcyu?apikey=yMslDnCVP4I9LZDZg25U1O29zwCvtGrR',
         'csv': 'https://dl.dropboxusercontent.com/u/7156802/apps/tools.csv'
     }
 ]
